197beb9df4c74cd72f496c9676c8b86795474b23
Stephen John Berenguer <stephenjohnberenguer@gmail.com>
2025-02-12 14:53:09 +0000

    feat: bring element

--- element.sh
+++ element.sh
@@ -1,3 +1,13 @@
-#! /bin/bash
+#!/bin/bash
 
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
+if [[ $1 ]]
+then
+  # if [[ ! $1 =~ ^[0-9]+$ ]]
+  # then
+  echo $1
+  # fi
+  else
+  echo "Please provide an element as an argument."
+Fi
